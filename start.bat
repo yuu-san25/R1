47e6682980840950dfdf30ba733bf441639a101a
yuu-san25 <syedibraheemshah25@gmail.com>
2026-02-13 21:25:28 +0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -8,6 +8,12 @@ net user HeadNode /active:yes
 :: Set RDP Registry keys just in case
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 
+:: Install and Start Dask Scheduler
+echo Installing Dask...
+pip install dask distributed --quiet
+echo Starting Dask Scheduler in background...
+start /b dask scheduler
+
 echo Successfully Installed!
 echo IP:
 "C:\Program Files\Tailscale\tailscale.exe" ip -4
